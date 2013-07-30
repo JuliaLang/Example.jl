99b03ed3a0d8ce98c0b67707fde62d91a2e313e5
Iain Dunning <iaindunning@gmail.com>
2013-07-29 23:10:17 -0700

    Incorporating Julep 2 changes

--- src/Example.jl
+++ src/Example.jl
@@ -5,4 +5,6 @@ importall Base
 hello(who::ASCIIString) = "Hello, $who"
 helloworld() = println(hello("World"))
 
+domath(x::Number) = (x + 5)
+
 end
--- test/test.jl
+++ test/test.jl
@@ -1,3 +1,16 @@
+using Base.Test
 require("Example")
 
-Test.@test Example.hello("Julia") == "Hello, Julia"
+test_type = length(ARGS) == 1 ? ARGS[1] : "ALL"
+
+if test_type == "ALL" || test_type == "TEST" || test_type == "INSTALL"
+  @test Example.hello("Julia") == "Hello, Julia"
+  @test Example.domath(2.0) == 7.0
+end
+
+if test_type == "ALL" || test_type == "PERF"
+  x = Example.domath(3.0)
+  @time for run = 1:1000000
+    x = Example.domath(run)
+  end
+end
