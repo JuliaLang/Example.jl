3ad6366a843e31fbc710f36d11692bd91b52128b
morris25 <sam.morrison@invenia.ca>
2018-10-26 14:50:35 -0500

    Adds docs

---
+++ docs/make.jl
@@ -0,0 +1,3 @@
+using Documenter, Example
+
+makedocs(modules = [Example])
--- src/Example.jl
+++ src/Example.jl
@@ -1,7 +1,18 @@
 module Example
 export hello, domath
 
+"""
+    hello(who::String)
+
+Returns "Hello, `who`"
+"""
 hello(who::String) = "Hello, $who"
+
+"""
+    domath(x::Number)
+
+Returns `x + 5`
+"""
 domath(x::Number) = x + 5
 
 end
