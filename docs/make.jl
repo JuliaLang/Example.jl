4ff3383186c051f2a1e79620897d4bbcf88fb136
morris25 <sam.morrison@invenia.ca>
2018-10-31 13:22:17 -0500

    Updates doc deployment, adds badges, and adds Documentation job to travis

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,9 @@
 using Documenter, Example
 
-makedocs(modules = [Example])
+makedocs(modules = [Example], sitename = "Example.jl")
 
 if get(ENV, "TEST_DOCS", "false") != "true"
     deploydocs(
         repo = "github.com/JuliaLang/Example.jl.git",
-        julia = "1.0"
     )
 end
