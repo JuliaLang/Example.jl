cd09f3e953dc1d431f632862f9ab7c9c20ac5ab1
morris25 <sam.morrison@invenia.ca>
2018-11-01 08:29:30 -0500

    Adds Manifest to gitignore and removes testing check

--- docs/make.jl
+++ docs/make.jl
@@ -2,8 +2,6 @@ using Documenter, Example
 
 makedocs(modules = [Example], sitename = "Example.jl")
 
-if get(ENV, "TEST_DOCS", "false") != "true"
-    deploydocs(
-        repo = "github.com/JuliaLang/Example.jl.git",
-    )
-end
+deploydocs(
+    repo = "github.com/JuliaLang/Example.jl.git",
+)
