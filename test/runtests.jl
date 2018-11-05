2486d6f9d5051969e51a26aa82fbfa6c7579402b
Alex Arslan <ararslan@comcast.net>
2018-11-05 14:16:22 -0800

    Test whether Travis coverage submission masks failures

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,8 @@ else
     using Test
 end
 
+# Test failure to ensure that Travis coverage submission doesn't mask failures
+@test 1 == 2
+
 @test hello("Julia") == "Hello, Julia"
 @test domath(2.0) ≈ 7.0
