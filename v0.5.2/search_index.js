e7c82705be8873c9f9d29ca7dfba9475e474a6eb
autodocs <autodocs>
2019-01-04 00:24:10 +0000

    build based on 66607a6

---
+++ v0.5.2/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Example",
+    "title": "Example",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Example.domath-Tuple{Number}",
+    "page": "Example",
+    "title": "Example.domath",
+    "category": "method",
+    "text": "domath(x::Number)\n\nReturn x + 5.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Example.hello-Tuple{String}",
+    "page": "Example",
+    "title": "Example.hello",
+    "category": "method",
+    "text": "hello(who::String)\n\nReturn \"Hello, who\".\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Example-1",
+    "page": "Example",
+    "title": "Example",
+    "category": "section",
+    "text": "Example Julia package repo.Modules = [Example]"
+},
+
+]}
