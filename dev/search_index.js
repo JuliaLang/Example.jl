28279e476d82b7d98f01a01def9e9f7a0064df95
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-09-05 07:55:25 +0000

    build based on 75fa835

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,35 +1,3 @@
-var documenterSearchIndex = {"docs": [
-
-{
-    "location": "#",
-    "page": "Example",
-    "title": "Example",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "#Example.domath-Tuple{Number}",
-    "page": "Example",
-    "title": "Example.domath",
-    "category": "method",
-    "text": "domath(x::Number)\n\nReturn x + 5.\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Example.hello-Tuple{String}",
-    "page": "Example",
-    "title": "Example.hello",
-    "category": "method",
-    "text": "hello(who::String)\n\nReturn \"Hello, who\".\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Example-1",
-    "page": "Example",
-    "title": "Example",
-    "category": "section",
-    "text": "Example Julia package repo.Modules = [Example]"
-},
-
-]}
+var documenterSearchIndex = {"docs":
+[{"location":"#Example-1","page":"Example","title":"Example","text":"","category":"section"},{"location":"#","page":"Example","title":"Example","text":"Example Julia package repo.","category":"page"},{"location":"#","page":"Example","title":"Example","text":"Modules = [Example]","category":"page"},{"location":"#Example.domath-Tuple{Number}","page":"Example","title":"Example.domath","text":"domath(x::Number)\n\nReturn x + 5.\n\n\n\n\n\n","category":"method"},{"location":"#Example.hello-Tuple{String}","page":"Example","title":"Example.hello","text":"hello(who::String)\n\nReturn \"Hello, who\".\n\n\n\n\n\n","category":"method"}]
+}
