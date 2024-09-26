d257c1fa73e73d6144b9bb85f4a13547c59fb80c
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-09-26 10:32:49 -0400

    Try to update the docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,14 @@
 using Documenter, Example
 
-makedocs(modules = [Example], sitename = "Example.jl")
+makedocs(modules = [Example],
+         sitename = "Example.jl",
+         format = Documenter.HTML()
+         )
 
-deploydocs(repo = "github.com/JuliaLang/Example.jl.git")
+deploydocs(
+    repo = "github.com/JuliaLang/Example.jl.git",
+    target = "build",
+    deps   = nothing,
+    make   = nothing,
+    push_preview = true,
+)
